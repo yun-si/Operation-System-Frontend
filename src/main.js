db8c0b785b6bd02f60716e8a712b98b6ca2f2677
yun-si <110306079@g.nccu.edu.tw>
2023-12-19 14:34:09 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import ElementPlus from "element-plus"
+import "element-plus/dist/index.css"
+import * as ElementPlusIconVue from "@element-plus/icons-vue"
+import App from './App.vue'
+
+const app = createApp(App)
+for (const[key, component] of Object.entries(ElementPlusIconVue)) {
+    app.component(key, component)
+}
+app.use(ElementPlus)
+
+app.mount('#app')
