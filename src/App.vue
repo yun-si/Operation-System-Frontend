5e31a51ea874a10086632d5755b85bb1c5384c23
yun-si <110306079@g.nccu.edu.tw>
2023-12-20 23:04:00 +0800

    Add Bar.vue - header and sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,21 @@
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-      <el-row class="mb-4">
-        <el-button>Default</el-button>
-        <el-button type="primary" :icon="Edit">Primary</el-button>
-        <el-button type="success" plain>Success</el-button>
-        <el-button type="info" round>Info</el-button>
-        <el-button type="warning">Warning</el-button>
-        <el-button type="danger" :icon="Edit"></el-button>
-      </el-row>
-    <!-- <TheWelcome /> -->
-  </main>
+    <Bar />
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import {
-  Check,
-  Delete,
-  Edit,
-  Message,
-  Search,
-  Star,
-} from '@element-plus/icons-vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import TheWelcome from './components/TheWelcome.vue'
+  import {
+    Check,
+    Delete,
+    Edit,
+    Message,
+    Search,
+    Star,
+  } from '@element-plus/icons-vue'
+  import Bar from './components/Bar.vue'
 </script>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
